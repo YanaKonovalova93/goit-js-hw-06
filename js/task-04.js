52d13bbaaeeaff3c83465bca4d531b5798586ffa
YanaKonovalova93 <mikobacteria@gmail.com>
2023-04-17 12:15:32 +0300

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,25 @@
+const divEl = document.querySelector('#counter');
+
+const buttonDecEl = document.querySelector('[data-action="decrement"]');
+const buttonIncEl = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+let counterValue = 0;
+
+const onDecrementValue = (() => {
+    counterValue -= 1;
+    console.log(counterValue);
+    valueEl.textContent = counterValue;
+  });
+
+  const onIncrementValue = (() => {
+    counterValue += 1;
+    console.log(counterValue);
+    valueEl.textContent = counterValue;
+  });
+
+buttonDecEl.addEventListener("click", onDecrementValue);
+
+buttonIncEl.addEventListener("click", onIncrementValue);
+
+
