8acaecce9932f9766b62946550e2bd0d956cbe24
YanaKonovalova93 <mikobacteria@gmail.com>
2023-04-17 11:14:36 +0300

    17.04.23

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,19 @@
-// numbers.forEach(function (number, index) {
-//     console.log(`Індекс ${index}, значення ${number}`);
-//   });
+const categoriesListEl = document.querySelector("#categories");
 
-// 1.Порахує і виведе в консоль кількість категорій в ul#categories, 
-// тобто елементів li.item.
-// 2.Для кожного элемента li.item у списку ul#categories, 
-// знайде і виведе в консоль текст заголовку елемента (тегу <h2>)
-//  і кількість елементів в категорії (усіх <li>, вкладених в нього).
+const liItemEl = categoriesListEl.querySelectorAll(".item");
 
+console.log(`Number of categories: ${liItemEl.length}`);
 
-//  1. нужно получить доступ к ли с классом ли-айтем
-// 2. потом перебрать лишки вложенные в ул(потомки)
-// 3. потом в кол-бэке приплюсовать их друг к другу
+liItemEl.forEach(function (item, index) {
 
+  const nameCategoriesEl = item.querySelector("h2");
+
+  const listEl = item.querySelector(".item ul");
+
+  const fistChildEl = listEl.querySelectorAll("li");
+
+  console.log(`Category: ${nameCategoriesEl.textContent}`);
+
+  console.log(`Elements: ${fistChildEl.length}`);
+  
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ul = document.querySelector("#ingredients");
+ingredients.forEach(function (ingredient, index) {
+
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+  console.log(liEl);
+  ul.append(liEl);
+
+});
+
+
