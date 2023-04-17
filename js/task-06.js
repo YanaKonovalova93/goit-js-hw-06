82a4e91a686ab2e091d0839147e8cba093c96458
YanaKonovalova93 <mikobacteria@gmail.com>
2023-04-17 17:23:42 +0300

    9 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,16 @@
-const inputEl = document.querySelector('#validation-input');
+const inputEl = document.querySelector("#validation-input");
 
-
-inputEl.addEventListener('blur', changeBorderInput)
+inputEl.addEventListener("blur", changeBorderInput);
 
 const inputText = inputEl.getAttribute("data-length");
 
 function changeBorderInput(event) {
+  inputEl.classList.remove("valid", "invalid");
 
-    inputEl.classList.remove("valid", "invalid");
+  const borderInput =
+    event.currentTarget.value.trim().length === Number(inputText)
+      ? "valid"
+      : "invalid";
 
-    const borderInput = event.currentTarget.value.trim().length === Number(inputText) ? "valid" : "invalid";
-    
-    inputEl.classList.add(borderInput);
-
+  inputEl.classList.add(borderInput);
 }
-
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+
+const divEl = document.querySelector(".widget");
+
+const spanEl = document.querySelector(".color");
+
+const buttonEl = document.querySelector(".change-color");
+
+buttonEl.addEventListener("click", onChangeColor);
+
+function onChangeColor(event) {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = `${randomColor}`;
+}
