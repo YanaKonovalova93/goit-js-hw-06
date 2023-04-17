06f79deed1e6d73e26c854ad744f04a5de0eede8
YanaKonovalova93 <mikobacteria@gmail.com>
2023-04-17 15:34:47 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const emailEl = { email: email.value };
+
+  const passwordlEl = { password: password.value };
+
+  const formObj = { ...emailEl, ...passwordlEl };
+
+  console.log(formObj);
+
+  event.currentTarget.reset();
+}
