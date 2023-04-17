131d4d5d5ef4166a708b8e7a5111314964133ca4
YanaKonovalova93 <mikobacteria@gmail.com>
2023-04-17 12:44:19 +0300

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector('#name-input');
+const spanEl = document.querySelector('#name-output');
+
+inputEl.addEventListener("input", onTextInput);
+
+function onTextInput(event) {
+    spanEl.textContent = inputEl.value === '' ? 'Anonymous' : event.currentTarget.value;
+}
+
+
